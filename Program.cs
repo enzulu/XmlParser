7f22fbe1d82911aa5863a04a4eae4b452031437d
enzulu <enzo.zulueta@gmail.com>
2021-05-11 09:20:53 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml;
+
+namespace XmlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var doc = new XmlDocument();
+            doc.Load(args[0]);
+
+            IterateThroughAllNodes(doc);
+        }
+
+        static private void IterateThroughAllNodes(XmlNode node)
+        {
+            Console.WriteLine(CreateIndent(node) + node.Name);
+
+            foreach (XmlNode childNode in node.ChildNodes)
+            {
+                IterateThroughAllNodes(childNode);
+            }
+        }
+
+        static private string CreateIndent(XmlNode node)
+        {
+            var outputIndent = String.Empty;
+
+            while (node.ParentNode != null)
+            {
+                outputIndent += "-";
+                node = node.ParentNode;
+            }
+
+            return outputIndent;
+        }
+    }
+}
